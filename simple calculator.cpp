c4b7dc4f2fb106a0b1615c188aa6f1101b12f149
victorszychovsky <160167332+victorszychovsky@users.noreply.github.com>
2024-12-13 19:03:49 +0100

    simple calculator.cpp
    added from cpp.sh

---
+++ simple calculator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+    int main() {    
+
+    int liczba1, liczba2;
+    cout << "Provide digits: ";
+    cin >> liczba1 >> liczba2; 
+    cout << liczba1 << " + " << liczba2 << " = " << liczba1 + liczba2 << endl;
+    cout << liczba1 << " - " << liczba2 << " = " << liczba1 - liczba2 << endl;
+    cout << liczba1 << " * " << liczba2 << " = " << liczba1 * liczba2 << endl;
+    cout << liczba1 << " / " << liczba2 << " = " << 1.0 * liczba1 / liczba2 << endl;
+    cout << liczba1 << " % " << liczba2 << " = " << liczba1 % liczba2 << endl;
+
+         return 0;
+    }
